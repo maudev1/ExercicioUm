f34c54a12f08fbefd70fea534a19fec31ca747d9
maudev-vgt <mauricio@vgt.com.br>
2023-11-16 21:00:10 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioUm/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // desafio 2
+            //double area = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //double pi = Math.PI;
+            //double area2 = area * area;
+            //double saida = area2 * pi;
+
+            //Console.WriteLine("Saida:{0:F7}", saida.ToString(CultureInfo.InvariantCulture));
+
+            //desafio 3
+            //Console.WriteLine("Digite 5 Numeros inteiros, divididos por espaço:");
+            //string Numeros = Console.ReadLine();
+            //string[] listaNumeros = Numeros.Split(' ');
+            //
+            //int numero1 = int.Parse(listaNumeros[0]);
+            //int numero2 = int.Parse(listaNumeros[1]);
+            //int numero3 = int.Parse(listaNumeros[2]);
+            //int numero4 = int.Parse(listaNumeros[3]);
+            //
+            //int diferenca = numero1 * numero2 - numero3 * numero4;
+            //
+            //Console.WriteLine("DIFERENÇA: {0}",diferenca);
+
+            //desafio 4
+
+            //int numero, totalHoras;
+            //double valorPorHora;
+            //    
+            //numero = int.Parse(Console.ReadLine());
+            //totalHoras = int.Parse(Console.ReadLine());
+            //valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //
+            //double total = totalHoras * valorPorHora;
+            //
+            //Console.WriteLine($"NUMBER = {numero}");
+            //Console.WriteLine($"SALARY = U$ {total.ToString("F2",CultureInfo.InvariantCulture)}");
+
+            //desafio 5
+
+            //string p1, p2;
+            //
+            //p1 = Console.ReadLine();
+            //p2 = Console.ReadLine();
+            //
+            //string[] peca1 = p1.Split(" ");
+            //string[] peca2 = p2.Split(" ");
+            //
+            //int cdPeca1 = int.Parse(peca1[0]);
+            //int numeroPeca1 = int.Parse(peca1[1]);
+            //double valorUnitPeca1 = double.Parse(peca1[2], CultureInfo.InvariantCulture);
+            //
+            //int cdPeca2 = int.Parse(peca2[0]);
+            //int numeroPeca2 = int.Parse(peca2[1]);
+            //double valorUnitPeca2 = double.Parse(peca2[2], CultureInfo.InvariantCulture);
+            //
+            //double total1 = numeroPeca1 * valorUnitPeca1;
+            //double total2 = numeroPeca2 * valorUnitPeca2;
+            //double total = total1+total2;
+            //
+            //
+            //Console.WriteLine($"R$ {total.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            //desafio 6
+
+            //string Entrada; 
+
+            //Entrada = Console.ReadLine();
+            //string[] Numeros = Entrada.Split(' '); 
+
+            //double A = double.Parse(Numeros[0], CultureInfo.InvariantCulture);
+            //double B = double.Parse(Numeros[1], CultureInfo.InvariantCulture);
+            //double C = double.Parse(Numeros[2], CultureInfo.InvariantCulture);
+
+            //double PI = Math.PI;
+
+
+            //// Área do Triangulo
+            //double triangulo = A * C / 2;
+
+            //// Área do Cículo
+
+            //double circulo = C * C * PI;
+
+            //// Área do Trapézio
+
+            //double trapezio = (A + B) / 2 * C;
+
+            //// Área do Quadrado
+
+            //double quadrado = B * B;
+
+            //// Área do Retângulo
+
+            //double retangulo = A * B;
+
+            //Console.WriteLine("TRIANGULO: "+triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            //Console.WriteLine("CIRCULO: "+circulo.ToString("F3", CultureInfo.InvariantCulture));
+            //Console.WriteLine("TRAPÉZIO: "+trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            //Console.WriteLine("QUADRADO: "+quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            //Console.WriteLine("RETANGULO: "+retangulo.ToString("F3", CultureInfo.InvariantCulture));
+
+
+        }
+
+    }
+
+
+}
